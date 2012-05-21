f6647aa08375cd731ce0c4a2ec63b0e285dbdec4
lpcabezas <lpcabezas@106-08.info2>
2012-05-21 13:37:42 +0200

    commit rama master

--- lpcabezas/src/clase2.java
+++ lpcabezas/src/clase2.java
@@ -4,7 +4,7 @@ public class clase2 {
 	/**
 	 * @param args
 	 */
-	
+	public voidmetodo rama master{}
 	public void metodoclase2{}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
