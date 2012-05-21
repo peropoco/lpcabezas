450bb92307ca7aec020de41a99ac9543c119176a
lpcabezas <lpcabezas@106-08.info2>
2012-05-21 13:30:41 +0200

    se le añaden a a las clases los metodos clase 1 y clase 2

--- lpcabezas/src/clase1.java
+++ lpcabezas/src/clase1.java
@@ -4,9 +4,12 @@ public class clase1 {
 	/**
 	 * @param args
 	 */
+	
+	public void metodo clase1{}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("soy la clase 1");
 	}
 
 }
--- lpcabezas/src/clase2.java
+++ lpcabezas/src/clase2.java
@@ -4,9 +4,11 @@ public class clase2 {
 	/**
 	 * @param args
 	 */
+	
+	public void metodoclase2{}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("soy la clase 2");
 	}
 
 }
